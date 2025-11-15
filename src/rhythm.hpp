e0abece6e3663c5079705ade5d254ee9eaf77a3e
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2025-11-15 16:48:25 -0700

    bring rhythm file up to feature parity with odin version

---
+++ src/rhythm.hpp
@@ -0,0 +1,43 @@
+#ifndef RHYTHM_H
+#define RHYTHM_H
+
+#include <raylib.h>
+#include <vector>
+
+enum Subdivision {
+  WHOLE,
+  HALF,
+  QUARTER,
+  EIGHTH,
+};
+
+struct Beat {
+  int count;
+  Subdivision subdivision;
+};
+
+struct Pattern {
+  std::vector<Beat> rhythm;
+  float time;
+};
+
+Subdivision patternGetCurrentSubdivision(Pattern p, float tempo);
+
+struct GetBeatResult {
+  int beatNumber;
+  float timeSinceBeat;
+};
+
+float duration(Pattern p, float tempo);
+float duration(Beat b, float tempo);
+float duration(Subdivision s, float tempo);
+
+float quarters(Pattern p);
+float quarters(Beat b);
+float quarters(Subdivision s);
+
+GetBeatResult getBeat(Music m, Subdivision subdivision, float tempo);
+
+float secondsPerBeat(float tempo);
+
+#endif
